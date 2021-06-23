f4bea6aedb8ad6136dbf8b7336f650518f314bab
Mateus Gambaro <magambaro@hotmail.com>
2021-06-23 18:40:03 -0300

    Testa o comando if, else if e else

--- bloco_4/dia_1/script.js
+++ bloco_4/dia_1/script.js
@@ -1,13 +1,14 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+let nota = "90";
 
-console.log (typeof patientId, typeof isEnrolled);
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado");
+}
 
-console.log (typeof patientInfo, typeof patientEmail);
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na lista de espera");
+}
 
 
+else (nota < 60) {
+    console.log("Você foi reprovado");
+}
