1672ec045b9b15ffd5ed4acf46743202c270f599
Mateus Gambaro <magambaro@hotmail.com>
2021-06-23 16:16:50 -0300

    Testa o comando typeof

--- bloco_4/dia_1/scripts.js
+++ bloco_4/dia_1/scripts.js
@@ -1,8 +1,12 @@
-const name = "Mateus";
-
-const birthCity = "Sorocaba";
-
-let birthYear = "1996";
-birthYear = "1990";
-
-console.log (name, birthCity, birthYear);
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+ 
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
