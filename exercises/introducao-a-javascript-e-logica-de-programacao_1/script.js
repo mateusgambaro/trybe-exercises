2cf859d11e627b300e319505885e7f5102b1bcb0
Mateus Gambaro <magambaro@hotmail.com>
2021-06-23 20:00:34 -0300

    Adiciona um programa que retorna o maior de dois número

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -20,3 +20,24 @@ console.log(multiplication);
 // Módulo
 let remainder = a % b;
 console.log(remainder);
+
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+function eMaior (a,b){
+
+if(a>b){
+    console.log("O maior número é ",a,"e o menor número é",b,"");
+}
+else if(a>b){
+    console.log("O maior número é ",a," e o menor é ",b,"");
+}
+else if(b>a){
+    console.log("O maior número é ",b," e o menor é ",a,"");
+}
+else if(b>a){
+    console.log("O maior número é ",b," e o menor é ",a,"");
+}
+
+}
+
+eMaior(a,b);
