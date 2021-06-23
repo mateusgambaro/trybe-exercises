255b289649d6c1bd77c6d480302c15ff765fc96e
Mateus Gambaro <magambaro@hotmail.com>
2021-06-23 19:31:31 -0300

    Adiciona a estrutura html em exercise_1 e adiciona o script.js com os 5 operadores lógicos

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -0,0 +1,22 @@
+let a = 5;
+let b = 10;
+
+// Soma
+let sum = (a + b);
+console.log(sum);
+
+// Subtração
+let subtraction = (a - b);
+console.log(subtraction);
+
+// Divisão
+let division = (a / b);
+console.log(division);
+
+// Multiplicação
+let multiplication = (a * b);
+console.log(multiplication);
+
+// Módulo
+let remainder = a % b;
+console.log(remainder);
