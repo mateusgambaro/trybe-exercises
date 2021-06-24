59ffc4132f841d36fb8552b630e33ac2e64d1896
Mateus Gambaro <magambaro@hotmail.com>
2021-06-24 18:45:43 -0300

    Adiciona um programa que retorna o maior de três números

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -1,5 +1,6 @@
-let a = 5;
+let a = 35;
 let b = 10;
+let c = 20;
 
 // Soma
 let sum = (a + b);
@@ -21,7 +22,7 @@ console.log(multiplication);
 let remainder = a % b;
 console.log(remainder);
 
-// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+// 2.  Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
 
 function eMaior (a,b){
 
@@ -41,3 +42,27 @@ else if(b>a){
 }
 
 eMaior(a,b);
+
+// 3. Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+
+
+
+function eMaiorTres (a,b,c){
+
+    if(a>b && a>c){
+        console.log("O maior número é ",a,"e o menor número é",b,"" || "O maior número é ",a,"e o menor número é",c,"" );
+    }
+    else if(a>b && a>c){
+        console.log("O maior número é ",a," e o menor é ",b,"" || "O maior número é ",a,"e o menor número é",c,"");
+    }
+    else if(b>a && b>c){
+        console.log("O maior número é ",b," e o menor é ",a,"" || "O maior número é ",b," e o menor é ",c,"");
+    }
+    else if(b>a && b>c){
+        console.log("O maior número é ",b," e o menor é ",a,"" || "O maior número é ",b," e o menor é ",c,"");
+    }
+    
+    }
+    
+    eMaiorTres (a,b,c);
+    
