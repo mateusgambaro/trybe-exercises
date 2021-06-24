0f02bb0c13fc8cf90c8c6adc2452e42845a996d7
Mateus Gambaro <magambaro@hotmail.com>
2021-06-24 18:58:19 -0300

    Adiciona programa que retorna se variável é positiva, negativa ou igual a zero

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -65,4 +65,17 @@ function eMaiorTres (a,b,c){
     }
     
     eMaiorTres (a,b,c);
-    
+
+    // 4. Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+    let numero = 3;
+
+    if (numero > 0){
+        console.log("positive");
+    }
+    else if (numero < 0){
+        console.log("negative");
+    }
+    else {
+        console.log ("zero");
+    }
