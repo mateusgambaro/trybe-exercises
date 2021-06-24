3c026d6f907bb8b735cf33df961fdeef64bc5e2e
Mateus Gambaro <magambaro@hotmail.com>
2021-06-24 19:24:59 -0300

    Adiciona programa que retorna true ou false para angulos válidos de um triângulo

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -79,3 +79,20 @@ function eMaiorTres (a,b,c){
     else {
         console.log ("zero");
     }
+
+    // 5. Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+    let angulo1 = 90;
+    let angulo2 = 60;
+    let angulo3 = 20;
+    let angulos = (angulo1 + angulo2 + angulo3);
+
+
+    if(angulo1 > 0 && angulo2 >0 && angulo3 > 0 && angulos == 180){
+        console.log(true);
+    }
+    else{
+        console.log (false);
+    }
+
+
