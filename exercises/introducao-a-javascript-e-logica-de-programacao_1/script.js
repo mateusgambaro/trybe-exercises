097c6c76ffe1dca3fdc52237f1bdd5465694c423
Mateus Gambaro <magambaro@hotmail.com>
2021-06-24 20:56:10 -0300

    Adiciona programa que retorna true se uma de três variáveis for par ou false se nenhuma for

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -145,3 +145,15 @@ function eMaiorTres (a,b,c){
         console.log("ERRO");
     }
 
+    // 8. Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+
+    let num1 = 1;
+    let num2 = 3;
+    let num3 = 2;
+
+    if ((num1 % 2 == 0) || (num2 % 2 == 0) || (num3 % 2 == 0)){
+        console.log(true);
+    }
+    else{
+        console.log(false);
+    }
