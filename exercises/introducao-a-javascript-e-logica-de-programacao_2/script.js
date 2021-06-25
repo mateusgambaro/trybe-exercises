2265a9bfd2af4033caab5ba8be72c761d8471f17
Mateus Gambaro <magambaro@hotmail.com>
2021-06-25 15:11:14 -0300

    Adiciona programa que soma todos os valores contido na array e imprime resultado

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
@@ -5,3 +5,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (index = 0; index < numbers.length; index += 1){
     console.log (numbers [index]);
 }
+
+// 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado
+
+let total = 0;
+
+for (index = 0; index < numbers.length; index += 1){
+    total += numbers[index];
+}
+
+console.log (total);
