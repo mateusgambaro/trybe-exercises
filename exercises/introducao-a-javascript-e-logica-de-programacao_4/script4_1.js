a74019c1fff06ee6432f1dce3af8669932c44b69
Mateus Gambaro <magambaro@hotmail.com>
2021-06-28 19:53:52 -0300

    Adiciona função array que retorna índice de maior valor

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script4_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script4_1.js
@@ -1,3 +1,5 @@
+// 1. Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
 let nomes = "arara";
 
 function verificaPalindrome (nomes){
@@ -14,3 +16,27 @@ function verificaPalindrome (nomes){
 }
 
 console.log(verificaPalindrome(nomes));
+
+// 2. Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+let arrayTest = [2, 3, 96, 7, 10, 1];
+
+function maxNumber(arrayTest){
+
+let maior = [];
+let indice = 0;
+
+for (let i = 1; i < arrayTest.length; i++){
+    if(arrayTest[i]>maior){
+        maior = arrayTest[i];
+        indice = i;
+    }
+}
+
+return indice;
+
+}
+
+
+
+console.log(maxNumber(arrayTest));
