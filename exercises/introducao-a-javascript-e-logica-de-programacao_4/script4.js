46fcec5ec0b62dd0f71f884d7372fbd06e8a93ef
Mateus Gambaro <magambaro@hotmail.com>
2021-06-28 15:24:35 -0300

    Executa For/In que mostra todas as chaves do objeto

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script4.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script4.js
@@ -12,3 +12,9 @@ let info = {
   // 2. Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
 
   console.log (info["recorrente"]);
+
+  // 3. Faça um for/in que mostre todas as chaves do objeto.
+
+  for(let key in info){
+      console.log(key);
+  }
