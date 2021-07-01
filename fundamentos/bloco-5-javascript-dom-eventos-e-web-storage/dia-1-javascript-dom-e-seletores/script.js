866c49733c8216a6fb18ef5751313bf64a132b4b
Mateus Gambaro <magambaro@hotmail.com>
2021-06-30 21:45:49 -0300

    Adiciona requisito #2 do exercício

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -3,3 +3,9 @@ console.log (document.getElementsByClassName(".main-content"));
 console.log (document.getElementsByTagName(".center-content p"));
 
 document.querySelector(".center-content p").innerText = "Pretendo estar trabalhando com tecnologia e sustentabilidade, em prol de um mundo melhor";
+
+function changeColor (elements, color){
+    document.querySelectorAll(elements)[0].style.backgroundColor = color;
+}
+
+changeColor (".center-content", "rgb(76,164,109)");
