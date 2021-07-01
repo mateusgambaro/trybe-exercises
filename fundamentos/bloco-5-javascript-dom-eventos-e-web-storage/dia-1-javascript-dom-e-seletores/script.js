d8c228dd513d618f317e231ca554e25d1e09d3f9
Mateus Gambaro <magambaro@hotmail.com>
2021-06-30 21:35:22 -0300

    Adiciona o requisito #1 do exercício

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,5 @@
+console.log (document.getElementById("title"));
+console.log (document.getElementsByClassName(".main-content"));
+console.log (document.getElementsByTagName(".center-content p"));
+
+document.querySelector(".center-content p").innerText = "Pretendo estar trabalhando com tecnologia e sustentabilidade, em prol de um mundo melhor";
