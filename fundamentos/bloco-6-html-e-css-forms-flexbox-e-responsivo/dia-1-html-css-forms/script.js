cc4a76cd5ec9916694d24b1135b827627073b28f
Mateus Gambaro <magambaro@hotmail.com>
2021-07-15 14:47:16 -0300

    Requisito 2 do exercício incompleto

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -3,6 +3,51 @@ let arrayStates = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','
 let getState = document.getElementById('input-state');
 
 arrayStates.forEach(function(element,key){
-    getState[key] =new Option(element, key);
+    getState[key] = new Option(element, key);
 });
+
+let getDate = document.querySelectorAll('#input-data');
+
+function validDate(getDate){
+    if(getDate.value.length === 0){
+      return {
+        message: 'A data não foi preenchida!'
+      }
+    }
+  
+    let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
     
+    if(!regex.test(getDate.value)){
+      return {
+        message: 'Data: Formato inválido'
+      };
+    }
+  
+    let splitted = getDate.value.split('/');
+    let day = splitted[0];
+    let month = splitted[1];
+    let year = splitted[2];
+  
+    if(day < 0 || day > 30){
+      return {
+        message: 'Dia inválido'
+      };
+    }
+  
+    if(month < 0 || month > 12){
+      return {
+        message: 'Mês inválido'
+      }
+    }
+  
+    if(year < 0) {
+      return {
+        message: 'Ano inválido'
+      };
+    }
+  
+    return true;
+  }
+
+    
+
