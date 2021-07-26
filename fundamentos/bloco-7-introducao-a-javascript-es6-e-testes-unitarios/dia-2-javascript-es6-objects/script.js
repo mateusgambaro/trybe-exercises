bbde11563ccfb2c0eb5ef24ab07e77cdb92e6289
Mateus Gambaro <magambaro@hotmail.com>
2021-07-26 14:11:16 -0300

    Adiciona exercicio 5,6 e 7 sobre objetos

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
@@ -96,3 +96,20 @@ const orderModifier = (order) => {
 
 const listKeys = (obj) =>  Object.keys(obj);
 console.log(listKeys(lesson1));
+
+// Crie uma função para mostrar o tamanho de um objeto.
+
+const sizeObj = (obj) =>  Object.keys(obj).length;
+console.log(sizeObj(lesson1));
+
+// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const listValues = (obj) =>  Object.values(obj);
+console.log(listValues(lesson1));
+
+// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+
+const allLessons = Object.assign({ lesson1, lesson2, lesson3 });
+
+console.log(allLessons);
+
