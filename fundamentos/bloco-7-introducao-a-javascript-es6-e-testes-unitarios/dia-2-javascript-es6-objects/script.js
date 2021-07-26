17894caf957f75d77d672e3b9cade36731e71291
Mateus Gambaro <magambaro@hotmail.com>
2021-07-26 14:17:03 -0300

    Adiciona exercicios 8, 9 e 10 sobre objetos

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
@@ -113,3 +113,31 @@ const allLessons = Object.assign({ lesson1, lesson2, lesson3 });
 
 console.log(allLessons);
 
+// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const getNumberOfStudents = (obj) => {
+    let total = 0;
+    const array = Object.keys(obj);
+    for (index in array) {
+      total += obj[array[index]].numeroEstudantes;
+    }
+    return total;
+  };
+  console.log(getNumberOfStudents(allLessons));
+
+  // Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+
+  const getValueByNumber = (obj,number) => Object.values(obj)[number];
+console.log(getValueByNumber(lesson2, 2));
+
+// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave
+
+const verifyPair = (obj, key, value) => {
+    const arr = Object.entries(obj);
+    let isEqual = false;
+    for (let index in arr) {
+      if (arr[index][0] === key && arr[index][1] === value) isEqual = true;
+    }
+    return isEqual;
+  };
+  console.log(verifyPair(lesson2,'professor','Carlos'));
