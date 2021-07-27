1362283c61ff2c75f04f370d07ffe8666610ce11
Mateus Gambaro <magambaro@hotmail.com>
2021-07-27 13:51:53 -0300

    Adiciona exercicio 4-Parte II sobre testes unitarios

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/script.js
@@ -148,7 +148,7 @@ assert.strictEqual(typeof wordLengths, 'function');
 const output = wordLengths(words);
 assert.deepStrictEqual(output, expected);*/
 
-const assert = require('assert');
+/*const assert = require('assert');
 // escreva a função sumAllNumbers aqui
 
 const sumAllNumbers = (array) => {
@@ -166,4 +166,32 @@ const expected = 53;
 const output = sumAllNumbers(numbers);
 
 assert.strictEqual(typeof sumAllNumbers, 'function');
+assert.strictEqual(output, expected);*/
+
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+
+const findTheNeedle = (array, word) => {
+    let output = -1;
+    for (const index in array) {
+      if (word === array[index]) {
+        output = Number(index);
+      }
+    }
+    return output;
+  };
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
 assert.strictEqual(output, expected);
