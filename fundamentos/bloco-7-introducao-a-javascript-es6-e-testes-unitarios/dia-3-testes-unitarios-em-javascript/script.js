d7497f66cf43ff288eb845352907cb108197d024
Mateus Gambaro <magambaro@hotmail.com>
2021-07-27 13:41:59 -0300

    Adiciona exercicio 3-Parte II sobre testes unitarios

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/script.js
@@ -130,7 +130,7 @@ assert.strictEqual(typeof addOne, 'function');
 assert.deepStrictEqual(output, expected);
 assert.deepStrictEqual(myArray, unchanged);*/
 
-const assert = require('assert');
+/*const assert = require('assert');
 // escreva a função wordLengths aqui
 
 const wordLengths = (array) => {
@@ -146,4 +146,24 @@ const expected = [3, 6, 10, 5];
 
 assert.strictEqual(typeof wordLengths, 'function');
 const output = wordLengths(words);
-assert.deepStrictEqual(output, expected);
+assert.deepStrictEqual(output, expected);*/
+
+const assert = require('assert');
+// escreva a função sumAllNumbers aqui
+
+const sumAllNumbers = (array) => {
+    let output = 0;
+
+    for (const item of array) {
+        output += item;
+    }
+
+    return output;
+};
+
+const numbers = [9, 23, 10, 3, 8];
+const expected = 53;
+const output = sumAllNumbers(numbers);
+
+assert.strictEqual(typeof sumAllNumbers, 'function');
+assert.strictEqual(output, expected);
