8d134b178e1fe251f9c27a30e66a68db40d9e44a
Mateus Gambaro <magambaro@hotmail.com>
2021-07-27 11:38:46 -0300

    Adiciona exercicio 3 sobre testes unitarios

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/script.js
@@ -19,7 +19,7 @@ assert.throws(() => {
     sum(4,'5');
 }, /^Error: parameters must be numbers$/);*/
 
-const assert = require('assert');
+/*const assert = require('assert');
 
 function myRemove(arr, item) {
   let newArr = [];
@@ -42,5 +42,32 @@ const myList = [5, 6, 7, 8];
 myRemove(myList, 5);
 assert.deepStrictEqual(myList, [5, 6, 7, 8]);
 
-assert.deepStrictEqual(myRemove(expected, 5), expected);
+assert.deepStrictEqual(myRemove(expected, 5), expected);*/
+
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// implemente seus testes aqui
+
+const expected = [1, 2, 3, 4];
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+myRemoveWithoutCopy(expected, 1);
+assert.strictEqual(expected, 3);
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
 
