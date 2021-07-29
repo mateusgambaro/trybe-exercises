6ea3e89db1d383c3922cddb0a221373a84667975
Mateus Gambaro <magambaro@hotmail.com>
2021-07-29 19:47:03 -0300

    Exercicio .find

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/paraFixar/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/paraFixar/script.js
@@ -46,11 +46,32 @@ const isEven2 = numbers.find((number) => number % 2 === 0);
 
 console.log(isEven2); // 30*/
 
-const numbers = [19, 21, 30, 3, 45, 22, 15];
+/*const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const findDivisibleBy3And5 = (number) => number % 3 === 0 && number % 5 === 0;
 
 const impar = numbers.find(findDivisibleBy3And5);
 
-console.log(impar);
+console.log(impar);*/
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = (name) => name.length === 5;
+  
+
+const nameEqualFive = names.find(findNameWithFiveLetters);
+
+console.log(nameEqualFive);
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ]
+  
+ const findMusic = (id) => {
+     return musicas.find((musica) => musica.id === id);
+ } 
+  
+ console.log(findMusic('31031685'));
 
