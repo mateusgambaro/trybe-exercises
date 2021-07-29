2999601e7a238791146fa8a69c17f14c19f62b61
Mateus Gambaro <magambaro@hotmail.com>
2021-07-29 19:29:02 -0300

    Exercicio paraFixar sobre .find

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/paraFixar/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/paraFixar/script.js
@@ -15,7 +15,7 @@ const convertToUpperCase = (name, index) => {
 names.forEach(convertToUpperCase);
 console.log(names);*/
 
-const emailListInData = [
+/*const emailListInData = [
     'roberta@email.com',
     'paulo@email.com',
     'anaroberta@email.com',
@@ -28,4 +28,29 @@ const emailListInData = [
   
   // Adicione seu código aqui
 
-  emailListInData.forEach(showEmailList);
+  emailListInData.forEach(showEmailList);*/
+
+  /*const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verifyEven = (xulambs) => xulambs % 2 === 0;
+
+const isEven = numbers.find(verifyEven);
+
+console.log(isEven); // 30
+
+console.log(verifyEven(9)); // False
+console.log(verifyEven(14)); // True
+
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+const isEven2 = numbers.find((number) => number % 2 === 0);
+
+console.log(isEven2); // 30*/
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (number) => number % 3 === 0 && number % 5 === 0;
+
+const impar = numbers.find(findDivisibleBy3And5);
+
+console.log(impar);
+
