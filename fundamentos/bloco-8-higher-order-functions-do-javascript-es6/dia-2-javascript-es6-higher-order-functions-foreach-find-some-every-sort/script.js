01ec1b2572c6bcfcb5059f68a710953a623793a1
Mateus Gambaro <magambaro@hotmail.com>
2021-07-29 21:50:25 -0300

    Adiciona exercicios 2 e 3 da lista de exercicios

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
@@ -72,3 +72,36 @@ function authorBornIn1947() {
   }
   
   assert.strictEqual(authorBornIn1947(), 'Stephen King');
+
+  // Retorne o nome do livro de menor nome.
+
+  function smallerName() {
+    let nameBook;
+    books.forEach((book) => {
+        if (!nameBook || book.name.length < nameBook.length) {
+          nameBook = book.name;
+        }
+      });
+    return nameBook;
+  }
+  
+  assert.strictEqual(smallerName(), 'Duna');
+
+  // Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+  const expectedResult = {
+    author: {
+      birthYear: 1948,
+      name: 'George R. R. Martin',
+    },
+    genre: 'Fantasia',
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    releaseYear: 1991,
+  };
+  
+  function getNamedBook() {
+    return books.find((book) => book.name.length === 26);
+  }
+  
+  assert.deepStrictEqual(getNamedBook(), expectedResult);
