241593891ff13299e084723c922a9d553445ffdc
Mateus Gambaro <magambaro@hotmail.com>
2021-07-29 21:30:45 -0300

    Adiciona exercicio sobre array.prototype.sort

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/paraFixar/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/paraFixar/script.js
@@ -75,7 +75,7 @@ const musicas = [
   
  console.log(findMusic('31031685'));*/
 
- const grades = {
+/* const grades = {
   portugues: 'Aprovado',
   matematica: 'Reprovado',
   ingles: 'Aprovado',
@@ -109,4 +109,31 @@ const verifyAges = (arr, minimumAge) => {
   return arr.every((person) => person.age >= minimumAge);
 }
 
-console.log(verifyAges(people, 18));
+console.log(verifyAges(people, 18));*/
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// Adicione se código aqui
+people.sort((personA, personB) => personA.age - personB.age);
+
+console.log(people);
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// Adicione se código aqui
+people.sort((personA, personB) => personB.age - personA.age);
+
+console.log(people);
+
