4a18f018054c0857174ad1d344a16c2f59b74521
Mateus Gambaro <magambaro@hotmail.com>
2021-08-01 20:19:30 -0300

    Adiciona exercicio 2 da lista de exercicios

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -139,7 +139,7 @@ const books = [
 
 // 1. Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
 
-const expectedResult = [
+/*const expectedResult = [
     'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
     'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
     'Fundação - Ficção Científica - Isaac Asimov',
@@ -152,4 +152,47 @@ const expectedResult = [
     return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
   }
   
-  assert.deepStrictEqual(formatedBookNames(), expectedResult);
+  assert.deepStrictEqual(formatedBookNames(), expectedResult);*/
+
+  // Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+
+  const expectedResult = [
+    {
+      age: 31,
+      author: 'Isaac Asimov',
+    },
+    {
+      age: 38,
+      author: 'H. P. Lovecraft',
+    },
+    {
+      age: 39,
+      author: 'Stephen King',
+    },
+    {
+      age: 43,
+      author: 'George R. R. Martin',
+    },
+    {
+      age: 45,
+      author: 'Frank Herbert',
+    },
+    {
+      age: 62,
+      author: 'J. R. R. Tolkien',
+    },
+  ];
+  
+  function nameAndAge() {
+    return books
+    .map((book) => (
+    {
+        author: book.author.name,
+        age: book.releaseYear - book.author.birthYear,
+
+    }
+    ))
+    .sort((obj1, obj2) => obj1.age - obj2.age);
+  }
+  
+  assert.deepStrictEqual(nameAndAge(), expectedResult);
