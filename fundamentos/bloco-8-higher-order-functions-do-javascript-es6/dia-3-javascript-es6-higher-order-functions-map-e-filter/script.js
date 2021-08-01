8999abbe321d0b93b3a0748d4c4e4402f9bbc633
Mateus Gambaro <magambaro@hotmail.com>
2021-08-01 20:12:46 -0300

    Adiciona exercicio 1 da lista de exercicios

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -1,4 +1,4 @@
-// Array .filter
+/*// Array .filter
 
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
@@ -59,10 +59,6 @@ console.log(numbers); // [ 1, 2, 3, 4, -5 ]
 const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
 const prices = [2.99, 3.99, 1.5, 2];
 
-const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
-
-const prices = [2.99, 3.99, 1.5, 2];
-
 const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
   { [product]: listPrices[index] }
 ));
@@ -74,4 +70,86 @@ console.log(listProducts);
 //   { Feijao: 3.99 },
 //   { Alface: 1.5 },
 //   { Tomate: 2 },
-// ]
+// ]*/
+
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// 1. Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+
+const expectedResult = [
+    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+    'Fundação - Ficção Científica - Isaac Asimov',
+    'Duna - Ficção Científica - Frank Herbert',
+    'A Coisa - Terror - Stephen King',
+    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  ];
+  
+  function formatedBookNames() {
+    return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+  }
+  
+  assert.deepStrictEqual(formatedBookNames(), expectedResult);
