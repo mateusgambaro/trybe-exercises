547479eb0f3fb03022511279f1fc5a70da2d44de
Mateus Gambaro <magambaro@hotmail.com>
2021-08-01 20:25:00 -0300

    Adiciona exercicio 3 da lista de exercicios

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -156,7 +156,7 @@ const books = [
 
   // Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
 
-  const expectedResult = [
+  /*const expectedResult = [
     {
       age: 31,
       author: 'Isaac Asimov',
@@ -195,4 +195,43 @@ const books = [
     .sort((obj1, obj2) => obj1.age - obj2.age);
   }
   
-  assert.deepStrictEqual(nameAndAge(), expectedResult);
+  assert.deepStrictEqual(nameAndAge(), expectedResult);*/
+
+  // 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
+  const expectedResult = [
+    { 
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: { name: 'George R. R. Martin', birthYear: 1948 },
+      releaseYear: 1991
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+      releaseYear: 1954
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: { name: 'Isaac Asimov', birthYear: 1920 },
+      releaseYear: 1951
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: { name: 'Frank Herbert', birthYear: 1920 },
+      releaseYear: 1965
+    }
+  ];
+  
+  function fantasyOrScienceFiction() {
+    return books.filter((genrer) => (genrer.genre === "Fantasia" || genrer.genre === "Ficção Científica"));
+  }
+  
+  assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
