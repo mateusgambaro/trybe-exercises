daced971c9ea9a1824c184575909c6861e3fb85a
Mateus Gambaro <magambaro@hotmail.com>
2021-08-10 12:22:22 -0300

    ParaFixar Callback

--- fundamentos/bloco-9-javascript-assincrono-e-promises/dia-1-javascript-assincrono-e-callbacks/paraFixar.js
+++ fundamentos/bloco-9-javascript-assincrono-e-promises/dia-1-javascript-assincrono-e-callbacks/paraFixar.js
@@ -24,15 +24,70 @@
 
 // const assert = require('assert');
 
-const pushNumber = (list, number) => {
-  list.push(number);
-  console.log(list);
+// const pushNumber = (list, number) => {
+//   list.push(number);
+//   console.log(list);
+// };
+
+// let numbers = [];
+
+// setTimeout(() => pushNumber(numbers, 1), 3000);
+// pushNumber(numbers, 2);
+// pushNumber(numbers, 3);
+
+// setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
+
+//--------CALLBACK-------------
+
+// const myExpenses = [
+//     {
+//       gym: 99,
+//     },
+//     {
+//       ifood: 200,
+//     },
+//     {
+//       phone: 60,
+//     },
+//     {
+//       internet: 100,
+//     },
+//   ];
+  
+//   const myIncome = 1000;
+  
+//   const monthlyBudget = (myIncome, myExpenses, callback) => {
+//     const totalExpenses = callback(myExpenses);
+//     const totalAfterExpenses = myIncome - totalExpenses;
+  
+//     console.log(`Balanço do mês:
+//       Recebido: R$${myIncome},00
+//       Gasto: R$${totalExpenses},00
+//       Saldo: R$${totalAfterExpenses},00 `);
+//   };
+  
+//   const handleExpenses = myExpenses => {
+//     const eachValue = myExpenses.map((item => Object.values(item)));
+//     const totalExpense = eachValue.reduce((acc, curr) => acc += curr[0] , 0);
+//     return totalExpense;
+//   };
+  
+//   monthlyBudget(myIncome, myExpenses, handleExpenses);
+
+const assert = require('assert');
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = callback => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn);
 };
 
-let numbers = [];
-
-setTimeout(() => pushNumber(numbers, 1), 3000);
-pushNumber(numbers, 2);
-pushNumber(numbers, 3);
-
-setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
+assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
+  
