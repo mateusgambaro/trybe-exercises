6f9dfdaa46616fe91d7fb1b14fb0f4f3032b2e52
Mateus Gambaro <magambaro@hotmail.com>
2021-08-19 20:43:52 -0300

    Adiciona exercicio com mesma funçao do ultimo push, porem, usando try e catch.

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/script.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/script.test.js
@@ -1,5 +1,6 @@
 // const uppercase = require('./script');
 // const findUserById = require('./script');
+const { describe, it, expect } = require('@jest/globals');
 const { getUserName }= require('./script');
 
 // it('should be uppercase "test" to equal "TEST"', (done) => {
@@ -13,18 +14,38 @@ const { getUserName }= require('./script');
 //     });
 //   });
 
-  describe('Find a username from a object with id number', () => {
-      describe('When user id exists', () => {
-          it('Return the username', () => (
-              getUserName(1).then((data) => expect(data).toEqual('Mark'))
-          ))
-      });
+//   describe('Find a username from a object with id number', () => {
+//       describe('When user id exists', () => {
+//           it('Return the username', () => (
+//               getUserName(1).then((data) => expect(data).toEqual('Mark'))
+//           ))
+//       });
 
-  describe('when the user id does not exists', () => {
-    it('returns an error', () => {
-      expect.assertions(1);
-      return getUserName(3).catch((error) =>
-        expect(error.message).toMatch('User with 3 not found.'));
+//   describe('when the user id does not exists', () => {
+//     it('returns an error', () => {
+//       expect.assertions(1);
+//       return getUserName(3).catch((error) =>
+//         expect(error.message).toMatch('User with 3 not found.'));
+//     });
+//   });
+// });
+
+describe('Find a username from a object with id number', () => {
+    describe('When user id exists', () => {
+        it('return the user name', async () => {
+            const data = await getUserName(1);
+            expect(data).toEqual('Mark')
+        });
     });
-  });
+
+    describe('when the user id does not exists', () => {
+        it('returns an error', async () => {
+          expect.assertions(1);
+          try {
+            await getUserName(3);
+          } catch (error) {
+            expect(error.message).toEqual('User with 3 not found.');
+          }
+        });
+      });
 });
