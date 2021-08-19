d657c36b8bbb1223bfd17fbf6628366b9dec470c
Mateus Gambaro <magambaro@hotmail.com>
2021-08-19 18:54:09 -0300

    Adiciona exercicio 1 sobre jest assincrono da lista de exercicios

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/script.test.js
@@ -0,0 +1,12 @@
+const uppercase = require('./script');
+
+it('should be uppercase "test" to equal "TEST"', (done) => {
+    uppercase('test', (str) => {
+      try {
+        expect(str).toBe('TEST');
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+  });
