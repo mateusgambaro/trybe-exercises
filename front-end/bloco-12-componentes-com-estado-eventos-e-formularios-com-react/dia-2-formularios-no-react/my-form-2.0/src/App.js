3e94cb300ca89ea06a16b6c32afb0e58c1fd5fd0
Mateus Gambaro <magambaro@hotmail.com>
2021-09-07 17:34:57 -0300

    Adiciona os 2 fieldset principais

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class Forms extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Formulário React</h1>
+        <fieldset>
+
+          <label>
+            Nome completo
+            <input name="nome" 
+            type="text" 
+            maxLength="40"
+            required
+            />
+          </label>
+
+          <label>
+            Email
+            <input name="email" 
+            type="text"
+            maxLength="50"
+            required
+            />
+          </label>
+
+          <label>
+            CPF
+            <input name="cpf" 
+            type="text"
+            maxLength="11"
+            required
+            />
+          </label>
+
+          <label>
+            Endereço
+            <textarea 
+            name="endereço" 
+            type="text"
+            maxLength="200"
+            required
+            />
+          </label>
+
+          <label>
+            Cidade
+            <textarea name="cidade" 
+            type="text"
+            maxLength="28"
+            required
+            />
+          </label>
+
+          <label>
+            Estado
+            <select name="estado" required/>
+            <option value =""></option>
+          </label>
+          
+          <label>
+            Casa
+            <input name="Casa-Apto"
+            type="radio"
+            required
+            />
+          </label>
+
+          <label>
+            Apartamento
+            <input name="Casa-Apto" 
+            type="radio"
+            required
+            />
+          </label>
+
+
+        </fieldset>
+
+        <fieldset>
+
+        <label>
+            Mini Currículo
+            <textarea name="cv" type="text" maxLength="1000" required/>
+          </label>
+
+          <label>
+            Cargo
+            <textarea name="job" type="text" maxLength="40" required/>
+          </label>
+
+          <label>
+            Descrição do cargo
+            <textarea name="job-description" type="text" maxLength="500" required/>
+          </label>
+
+        </fieldset>
+
+        <button>Enviar</button>
+        <button>Limpar</button>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Forms
