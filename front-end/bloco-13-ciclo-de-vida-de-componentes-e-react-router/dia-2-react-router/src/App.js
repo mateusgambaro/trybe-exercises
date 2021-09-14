0886c11350c3b0fa2232a8929551b6b481cc5e62
Mateus Gambaro <magambaro@hotmail.com>
2021-09-13 22:19:18 -0300

    Exercise 4 and 5

--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/src/App.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/users" component={Users} />
       </BrowserRouter>
     );
   }
