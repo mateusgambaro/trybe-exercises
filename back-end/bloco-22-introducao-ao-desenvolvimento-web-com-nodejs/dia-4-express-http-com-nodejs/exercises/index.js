f5f962c0e5199958202dc69ac5d40b45cd88c3fa
Mateus Gambaro <magambaro@hotmail.com>
2022-01-13 15:39:37 -0300

    exercicio 2 com body-parser

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
@@ -1,13 +1,13 @@
 /* index.js */
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
-const message = [
-  { message: 'pong' }
-];
+app.use(bodyParser.json());
 
-app.get('/ping', function (req, res) {
-  res.json(message);
+app.post('/hello', (req, res) => {
+  const { name }  = req.body;
+  res.status(200).json({ "message": `Hello, ${name}!` })
 });
 
 app.listen(3001, () => {
