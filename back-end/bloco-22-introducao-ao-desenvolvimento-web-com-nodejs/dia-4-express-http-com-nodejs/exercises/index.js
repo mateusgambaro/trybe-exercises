b9007b48d833bf30fbba0ec6370af8d657567da6
Mateus Gambaro <magambaro@hotmail.com>
2022-01-13 15:50:58 -0300

    exercicio 3 usando condicoes (if)

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
@@ -10,6 +10,15 @@ app.post('/hello', (req, res) => {
   res.status(200).json({ "message": `Hello, ${name}!` })
 });
 
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if( parseInt(age) > 17 ) {
+    return res.status(200).json({'message': `Hello, ${name}! `})
+  }
+   res.status(401).json({'message': `Unauthorized`});
+})
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
