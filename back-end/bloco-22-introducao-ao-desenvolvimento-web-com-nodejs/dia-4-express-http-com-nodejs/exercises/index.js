b08e9054d623eaca28deb8145a27a58596afbba1
Mateus Gambaro <magambaro@hotmail.com>
2022-01-13 16:05:56 -0300

    exercicio 4 com req.params

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
@@ -5,18 +5,24 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/hello', (req, res) => {
+/* app.post('/hello', (req, res) => {
   const { name }  = req.body;
   res.status(200).json({ "message": `Hello, ${name}!` })
-});
+}); */
 
-app.post('/greetings', (req, res) => {
+/* app.post('/greetings', (req, res) => {
   const { name, age } = req.body;
 
   if( parseInt(age) > 17 ) {
     return res.status(200).json({'message': `Hello, ${name}! `})
   }
    res.status(401).json({'message': `Unauthorized`});
+}) */
+
+app.put('/users/:name/:age', (req,res) => {
+  const { name, age } = req.params;
+
+  res.status(200).json({"message": `Seu nome é ${name} e você tem ${age} anos de idade`})
 })
 
 app.listen(3001, () => {
