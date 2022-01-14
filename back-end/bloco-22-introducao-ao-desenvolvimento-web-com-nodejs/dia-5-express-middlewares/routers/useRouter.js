a03db1bed3dc6a5f2aefaf052114691bc3e89e1d
Mateus Gambaro <magambaro@hotmail.com>
2022-01-14 15:40:52 -0300

    ex 2. add routers and changes on index.js

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/routers/useRouter.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/routers/useRouter.js
@@ -4,3 +4,18 @@ const {
   isValidEmail,
   isValidPassword,
 } = require('../middlewares/validations');
+
+router.post( '/register', 
+isValidUserName,
+isValidEmail,
+isValidPassword,
+(_req, res) => res.status(201).json({message: 'user created'}),
+);
+
+router.post( '/login',
+isValidEmail,
+isValidPassword,
+(_req, res) => res.status(200).json({token: '86567349784e'})
+);
+
+module.exports = router;
