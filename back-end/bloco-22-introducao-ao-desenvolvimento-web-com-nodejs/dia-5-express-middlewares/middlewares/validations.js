7b7047c20e2270e6413744e5aa67be4ab71698d8
Mateus Gambaro <magambaro@hotmail.com>
2022-01-14 15:08:41 -0300

    ex add password validation

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/middlewares/validations.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/middlewares/validations.js
@@ -11,7 +11,17 @@ const isValidEmail = (req, res, next) => {
   const { email } = req.body;
 
   if(!email || !email.includes('@') || !email.includes('.com'))
-    return res.status(400),json({message : 'invalid data'});
+    return res.status(400).json({message : 'invalid data'});
 
     next();
 };
+
+const isValidPassword = (req, res, next) => {
+  const { password } = req.body;
+  const passwordRegex = /^[0-9]*$/;
+
+  if(!password.match(/^[0-9]*$/) || password < 3 || password > 8);
+    return res.status(400).json({message : 'invalid data'});
+
+    next();
+}
